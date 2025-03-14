a48bfe3da7bec28e68e8a40ef55dd6853aedb030
Jef <jdovalov@gmail.com>
2025-03-13 21:56:49 -0400

    Add filtering by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -38,8 +38,15 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    booksByAuthor = {};
+    Object.keys(books).forEach((isbn)=>{
+        if ( books[isbn].author.toLowerCase()==author.toLowerCase() )
+        {
+            booksByAuthor[isbn]=books[isbn];
+        }
+    });
+    return res.send(JSON.stringify(booksByAuthor));
 });
 
 // Get all books based on title
