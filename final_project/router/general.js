b2a8d9ca0679edc5e12d59cf3f5a89e31f10ebc3
Jef <jdovalov@gmail.com>
2025-03-13 21:29:12 -0400

    Implement looku by isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,13 +27,13 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
+    return res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.send(books[isbn]);
  });
   
 // Get book details based on author
